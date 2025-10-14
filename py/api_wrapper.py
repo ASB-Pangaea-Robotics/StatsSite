f1ed1c0ff8c072ca2cf0c5965b48ad68e8e8f8e2
diabloxtreme666 <thediabloxtreme666@gmail.com>
2025-10-14 15:22:40 +0530

    API is now working. Previous one has been removed after a lot of struggles, and the new one is now working.

--- py/api_wrapper.py
+++ py/api_wrapper.py
@@ -1,10 +1,30 @@
-import dotenv
-import base64
+import datetime
+import json
 import os
+from unittest import case
 
-class FTC_API:
-    def __init__(self):
-        dotenv.load_dotenv()
-        token_str = f"{os.getenv("USER")}:{os.getenv("AUTH_TOKEN")}"
-        self.AUTH = base64.b64encode(token_str.encode()).decode("utf-8")
+import requests
+import dotenv
+
+# Deprecated since that API is no longer being used. Left in case we do switch back to it.
+#
+# dotenv.load_dotenv()
+#
+# token = os.getenv("TOKEN")
+
+
+url = "https://api.ftcscout.org/rest/v1"
+test_id = 21971
+
+
+def get_team_data(url: str,team_id: int, operation: str, *args: list[str]) -> dict:
+    match operation:
+        case "general":
+            return dict(requests.get(f"{url}/teams/{team_id}").json())
+        case "previous_season_stats":
+                return dict(requests.get(f"{url}/teams/{team_id}/quick-stats?season={str(int(datetime.datetime.now().year)-1)}").json())
+        case _:
+            return {"fail": "Please give a valid operation"}
+
+print(get_team_data(url,21971, "previous_season_stats"))
 
