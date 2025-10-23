5169c49096e946258f0ef4531bd6f2d8244ed701
nnawonk1m <nnawonk1m@gmail.com>
2025-10-23 19:59:32 +0530

    updated main.py to include search.html page

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('landing.html')
+    return flask.render_template('index.html')
 
 @app.route('/insights')
 def insights():
     return flask.render_template('insights.html')
+
+@app.route('/search')
+def search():
+    return flask.render_template('search.html')
 
 
 app.run(debug=True)
